98371a3b9ac7f279bbcd1b9d83d8e9afbb3abcc9
emils.kadikis <emils.kadikis@resourcify.de>
2023-05-23 08:04:50 +0200

    FGraDA All 4 domains

---
+++ FGraDA/data_loader_phone_zh_en.py
@@ -0,0 +1,12 @@
+def load_data():
+    target_texts = []
+    with open("./FGraDA/FDMT3.0/dev/phone.dev.en", "r") as f:
+        for line in f:
+            target_texts.append(line.strip())
+
+    source_texts = []
+    with open("./FGraDA/FDMT3.0/dev/phone.dev.zh", "r") as f:
+        for line in f:
+            source_texts.append(line.strip())
+
+    return source_texts, target_texts
