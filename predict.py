9aa606bd6ca45d7748d05a19bccc6448d6e5367b
emils.kadikis <emils.kadikis@resourcify.de>
2023-07-02 13:41:27 +0200

    Batched unadapted prediction generation

--- predict.py
+++ predict.py
@@ -17,12 +17,10 @@ def make_predictions(source_texts, max_length=100, output_file_name=None, model_
     tokenizer = MarianTokenizer.from_pretrained(model_name)
 
     predictions = []
-    for text in tqdm(source_texts):
-        input_tensor = tokenizer(text, return_tensors="pt").input_ids.to(device)
-        config = GenerationConfig(num_beams=1, do_sample=False, max_length=max_length)
-        prediction = model.generate(input_tensor, generation_config=config)
-        prediction = tokenizer.decode(prediction[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        predictions.append(prediction)
+    tokenizer_output = tokenizer(source_texts, return_tensors="pt", padding=True)
+    config = GenerationConfig(num_beams=1, do_sample=False, max_length=max_length)
+    predictions = model.generate(tokenizer_output.input_ids, attention_mask=tokenizer_output.attention_mask, generation_config=config)
+    predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
     # write predictions to file
     if output_file_name is not None:
@@ -31,3 +29,15 @@ def make_predictions(source_texts, max_length=100, output_file_name=None, model_
                 f.write(prediction + "\n")
     
     return predictions
+
+
+if __name__ == '__main__':
+    from formality.data_loader_formal_telephony import load_data
+    source_texts, target_texts, _, _ = load_data("en", "de")
+    predictions = make_predictions(source_texts, model_name="Helsinki-NLP/opus-mt-en-de")
+    print(len(predictions))
+    print(predictions[0])
+    print(predictions[1])
+    print(predictions[2])
+    print(predictions[3])
+    print(predictions[4])
