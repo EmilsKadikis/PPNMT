cb0d5311ecdf5869fa501319c04f50dbd405cf26
emils.kadikis <emils.kadikis@resourcify.de>
2023-07-22 18:30:23 +0200

    Moved out shared hyperparameters and other configurations to a base.json file.

--- experiment_helpers.py
+++ experiment_helpers.py
@@ -1,6 +1,7 @@
 from itertools import product
+import json
 
-def product_dict(**kwargs):
+def _product_dict(**kwargs):
     """
     Given a dictionary such as {a: [1,2,3], b: [4,5,6]}
     Iterates through all possbiel combinations: [{a: 1, b: 4}, {a: 1, b: 5}, {a: 1, b: 6}, ....]
@@ -12,20 +13,46 @@ def product_dict(**kwargs):
     for instance in product(*vals):
         yield dict(zip(keys, instance))
 
+def _merge_configs(base_config, extra_config):
+    merged_config = base_config.copy()
+
+    for key, value in extra_config.items():
+        if isinstance(value, dict) and key in base_config and isinstance(base_config[key], dict):
+            # Recursively merge nested dictionaries
+            merged_config[key] = _merge_configs(base_config[key], value)
+        else:
+            # Override or add the key-value pair to the merged configuration
+            merged_config[key] = value
+
+    return merged_config
+
+def _extend_experiment_definition_from_base(experiment_definition):
+    base_file_name = experiment_definition.pop("base", None)
+    if base_file_name is None:
+        return experiment_definition
+    
+    with open(base_file_name, "r") as f:
+        base_config = json.load(f)
+
+    return _merge_configs(base_config, experiment_definition)
 
 def expand_experiments(experiments):
     """
+    If the experiment has the key "base" defined, it is used as the base for all the configurations defined in "experiments".
+
     For every experiment that has 'try_combinations': [column_a, column_b, ...] defined, 
     expands it into multiple separate experiments, trying all possible combinations of the values given in column_a, column_b, ....
     """
     if type(experiments) is not list:
         experiments = [experiments]
+    experiments = [_extend_experiment_definition_from_base(experiment) for experiment in experiments]
+
     expanded_experiments = []
     for experiment_definition in experiments:
         if 'try_combinations' in experiment_definition:
             column_names = experiment_definition['try_combinations']
             all_possible_values = {column_name: experiment_definition['hyperparameters'][column_name] for column_name in column_names}
-            all_possible_value_combinations = product_dict(**all_possible_values)
+            all_possible_value_combinations = _product_dict(**all_possible_values)
 
             # create a new experiment definition for each combination
             for combination in all_possible_value_combinations:
@@ -40,3 +67,4 @@ def expand_experiments(experiments):
         else:
             expanded_experiments.append(experiment_definition)
     return expanded_experiments
+
