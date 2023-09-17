de695aafb9aab4ccba83c85fccdbbd01e9beafac
emils.kadikis <emils.kadikis@resourcify.de>
2023-09-17 20:17:34 +0200

    Splitting FGraDA by sentences, updated data loader with more BoW options

--- fine_grained_tech/data_loader.py
+++ fine_grained_tech/data_loader.py
@@ -1,10 +1,10 @@
 possible_source_languages = ["en", "zh"]
 possible_target_languages = ["en", "zh"]
-possible_domains = ["auto", "education", "network", "phone"]
-possible_bag_of_words_types = ["dict", "topic_modeling"]
+possible_domains = ["auto", "education", "network", "phone", "auto-sentences", "education-sentences", "network-sentences", "phone-sentences"]
+possible_bag_of_words_types = ["dict", "topic_modeling", "contrastive", "contrastive_dict"]
 possible_splits = ["dev", "test"]
 
-def load_data(source_language, target_language, split, domain, bag_of_words_type, use_negative_bags_of_words=False):
+def load_data(source_language, target_language, split, domain, bag_of_words_type, use_negative_bags_of_words=False, bag_of_words_size=10):
     if source_language not in possible_source_languages:
         raise ValueError("Invalid source language: " + source_language)
     if target_language not in possible_target_languages:
@@ -26,6 +26,10 @@ def load_data(source_language, target_language, split, domain, bag_of_words_type
         positive_bag_of_words, negative_bag_of_words = _get_dict_bag_of_words(domain, target_language)
     elif bag_of_words_type == "topic_modeling":
         positive_bag_of_words, negative_bag_of_words = _get_topic_modeling_bag_of_words(domain, split, target_language)
+    elif bag_of_words_type == "contrastive":
+        positive_bag_of_words, negative_bag_of_words = _get_contrastive_bag_of_words(domain, split, target_language, bag_of_words_size)
+    elif bag_of_words_type == "contrastive_dict":
+        positive_bag_of_words, negative_bag_of_words = _get_contrastive_dict_bag_of_words(domain, split, target_language, bag_of_words_size)
     else:
         raise ValueError("Invalid bag of words type: " + bag_of_words_type)
     
@@ -45,9 +49,13 @@ def _load_domain_texts(domain, split, language):
 
 
 def _get_dict_bag_of_words(domain, target_language):
+    if "sentences" in domain:
+        domain = domain.split("-")[0]
     positive_bag_of_words = f"fine_grained_tech/FDMT3.0/dict/{domain}.dict.{target_language}"
     negative_bag_of_words = ""
     for other_domain in possible_domains:
+        if "sentences" in other_domain:
+            continue
         if other_domain != domain:
             negative_bag_of_words += f"fine_grained_tech/FDMT3.0/dict/{other_domain}.dict.{target_language};"
     negative_bag_of_words = negative_bag_of_words[:-1]
@@ -59,6 +67,10 @@ def _get_topic_modeling_bag_of_words(domain, split, target_language):
     domain_texts = [_load_domain_texts(domain, split, target_language)]
 
     for other_domain in possible_domains:
+        if "sentences" not in domain and "sentences" in other_domain:
+            continue
+        elif "sentences" in domain and "sentences" not in other_domain:
+            continue
         if other_domain != domain:
             domain_texts.append(_load_domain_texts(other_domain, split, target_language))
             
@@ -66,6 +78,54 @@ def _get_topic_modeling_bag_of_words(domain, split, target_language):
     negative_bags_of_words = bags_of_words[1:]
     negative_bag_of_words = [word for bag_of_words in negative_bags_of_words for word in bag_of_words]
     return bags_of_words[0], negative_bag_of_words
+
+def _get_contrastive_bag_of_words(domain, split, target_language, max_bag_size=10):
+    from generate_bag_of_words_contrastive import generate_bag_of_words as generate_bag_of_words_contrastive
+    
+    domain_texts = [_load_domain_texts(domain, split, target_language)]
+    for other_domain in possible_domains:
+        if "sentences" not in domain and "sentences" in other_domain:
+            continue
+        elif "sentences" in domain and "sentences" not in other_domain:
+            continue
+        if other_domain != domain:
+            domain_texts.append(_load_domain_texts(other_domain, split, target_language))
+
+    tokenizer_model = "Helsinki-NLP/opus-mt-en-zh" if target_language == "zh" else "Helsinki-NLP/opus-mt-zh-en"
+    bags_of_words = generate_bag_of_words_contrastive(tokenizer_model, max_bag_size, domain_texts)
+    negative_bags_of_words = bags_of_words[1:]
+    negative_bag_of_words = [word for bag_of_words in negative_bags_of_words for word in bag_of_words]
+    return bags_of_words[0], negative_bag_of_words
+
+def _get_contrastive_dict_bag_of_words(domain, split, target_language, max_bag_size=10):
+    if "sentences" in domain:
+        domain = domain.split("-")[0]
+    from generate_bag_of_words_contrastive import generate_bag_of_words as generate_bag_of_words_contrastive
+    positive_dict_path = f"fine_grained_tech/FDMT3.0/dict/{domain}.dict.{target_language}"
+    positive_words = []
+    with open(positive_dict_path, "r") as f:
+        for line in f:
+            positive_words.append(line.strip())
+
+    domain_texts = [positive_words]
+    for other_domain in possible_domains:
+        if "sentences" in other_domain:
+            continue
+        if other_domain != domain:
+            negative_words = []
+            negative_dict_path = f"fine_grained_tech/FDMT3.0/dict/{other_domain}.dict.{target_language}"
+            with open(negative_dict_path, "r") as f:
+                for line in f:
+                    negative_words.append(line.strip())
+
+            domain_texts.append(negative_words)
+
+    tokenizer_model = "Helsinki-NLP/opus-mt-en-zh" if target_language == "zh" else "Helsinki-NLP/opus-mt-zh-en"
+    bags_of_words = generate_bag_of_words_contrastive(tokenizer_model, max_bag_size, domain_texts)
+    negative_bags_of_words = bags_of_words[1:]
+    negative_bag_of_words = [word for bag_of_words in negative_bags_of_words for word in bag_of_words]
+    return bags_of_words[0], negative_bag_of_words
+
 
 if __name__ == "__main__":
     # examples
@@ -78,6 +138,14 @@ if __name__ == "__main__":
     print("------------------")
 
     source_texts, target_texts, positive_bag_of_words, negative_bag_of_words = load_data("zh", "en", "dev", "education", "topic_modeling", True)
+    print(source_texts[0])
+    print(target_texts[0])
+    print(positive_bag_of_words)
+    print(negative_bag_of_words)    
+    
+    print("------------------")
+
+    source_texts, target_texts, positive_bag_of_words, negative_bag_of_words = load_data("zh", "en", "dev", "education", "contrastive_dict", True)
     print(source_texts[0])
     print(target_texts[0])
     print(positive_bag_of_words)
